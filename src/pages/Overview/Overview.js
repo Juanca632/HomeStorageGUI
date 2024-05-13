463a021dad9712a9f1cdeaad5434602e8dcc0b59
juanca632 <juanca632@hotmail.com>
2024-05-13 16:53:30 +0200

    emoji page modified

--- src/pages/Overview/Overview.js
+++ src/pages/Overview/Overview.js
@@ -12,179 +12,128 @@ import solarPanel from "../../assets/img/solar-panel.svg"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import {  DonutChart, Legend } from '@tremor/react';
+import {  DonutChart, Legend, BarChart } from '@tremor/react';
 
-const sales = [
+const data = [
     {
-      name: 'Eau chaude',
-      sales: 9.5,
+      date: 'Janvier',
+      'Epargne': 20,
     },
     {
-      name: 'Électroménager',
-      sales: 16.3,
+      date: 'Fevrier',
+      'Epargne': 40,
     },
     {
-      name: 'Cuisson',
-      sales: 0.4,
+      date: 'Mars',
+      'Epargne': 10,
     },
     {
-      name: 'Audiovisuel',
-      sales: 1.3,
+      date: 'Avril',
+      'Epargne': 5,
     },
     {
-      name: 'Éclairage',
-      sales: 4.2,
+      date: 'Mai',
+      'Epargne': 15,
     },
     {
-      name: 'Autres',
-      sales: 3.4,
+      date: 'June',
+      'Epargne': 8,
     },
     {
-      name: 'Chauffage',
-      sales: 16.8,
+      date: 'Julliet',
+      'Epargne': 2,
+    }
+  ];
+const productionType = [
+    {
+      name: 'Solaire',
+      sales: 80,
+    },
+    {
+      name: 'Heure Cleuse/Pleine',
+      sales: 20,
     }
   ];
 
+  const dataFormatter = (number) => {
+    // Formatea el valor numérico
+    const formattedValue = Intl.NumberFormat('en-US', {
+      maximumFractionDigits: 1,
+      notation: 'compact',
+      compactDisplay: 'short',
+    }).format(number);
+  
+    // Concatena "kWh" al valor formateado
+    return `€${formattedValue}`;
+  };
+
+const totalValue = productionType.reduce((acc, curr) => acc + curr.sales, 0);
+
+const dataFormatter2 = (number) => {
+    const percentage = (number / totalValue) * 100;
+    return `${percentage.toFixed(2)}%`;
+  };
 
 const Overview = () => {
 
     const navigate = useNavigate();
-  
+
+
     const swipeHandlers = useSwipeable({
       onSwipedRight: () => {
         // Navegar a la ruta "/charts" al detectar un gesto de swipe hacia la derecha
         navigate('/');
-      },
+      }
     });
 
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    const dataFormatter = (number) => {
-        // Formatea el valor numérico
-        const formattedValue = Intl.NumberFormat('en-US', {
-          maximumFractionDigits: 1,
-          notation: 'compact',
-          compactDisplay: 'short',
-        }).format(number);
-      
-        // Concatena "kWh" al valor formateado
-        return `€${formattedValue}`;
-      };
 
 
     return (
-        
+        <div className='overview' {...swipeHandlers}>
+            <div className='overview-div-columns'>
+                <div className='overview-div-rows overview-div-rows overview-row-1'>
+                <img src={emoji} alt="Emoji" className='emoji'/>
+                <p className='p-emoji'>Tout marche bien!</p>
+                </div>
+                <div className='overview-div-rows overview-row-2'>
+                        <DonutChart
+                            data={productionType}
+                            category="sales"
+                            index="name"
+                            variant="pie"
+                            valueFormatter={dataFormatter2}
+                            colors={['yellow', 'blue', 'indigo', 'violet', 'fuchsia', 'red', 'green']}
+                            className="w-40 donut-chart"
+                          />
+                          <Legend
+                            categories={['Solaire','Heure Cleuse/Pleine']}
+                            colors={['yellow', 'blue', 'indigo', 'violet', 'fuchsia', 'red', 'green']}
+                            className="max-w-xs"
+                          />
+                </div>
 
-        
-        <div {...swipeHandlers} className='overview'>
-
-            <div className='overview-box overview-box1'>
-            <div className='overview-box1-info'>
-                <img src={solarPanel} alt="emoji" className=''></img>
-                <p>539 W</p>
             </div>
-            <CircularProgressbar
-                    value={80}
-                    className='circular-progressbar1'
-                    circleRatio={0.4}
-                    styles={{
-                        trail: {
-                            
-                            transform: "rotate(-162deg)",
-                            transformOrigin: "center center"
-                        },
-                        path:{
-                            
-                            transform: "rotate(-162deg)",
-                            transformOrigin: "center center",
-                            stroke: "#FFC000"
-                        },
-                        text:{
-                            fill: "#ddd"
-                        }
-                    }}
-                    strokeWidth={5}
-                />
-            </div>
-            <div className='overview-box'>
-                    <ProgressCircle value={75} size="xl" color="blue" showAnimation="true">
-                        <div className='progress-circle-div'>
-                            <div className='progress-circle-box1'>
-                                <img src={battery} alt="battery" className=''></img>
-                                <p>Batterie</p>
-                            </div>
-                            <div className='progress-circle-box2'>
-                                <img src={emoji} alt="emoji" className='emoji'></img>
-                            </div>
-                            <div className='savings'>
-                                <Button variant="success" onClick={handleShow}>
-                                    Mon epargne
-                                </Button>
-
-                                <Modal show={show} onHide={handleClose} centered={true}>
-                                    <Modal.Header closeButton>
-                                    <Modal.Title>Mon epargne en cours du mois</Modal.Title>
-                                    </Modal.Header>
-                                    <Modal.Body>
-                                    <div className="b3-box">
-                                        <div className="flex justify-start space-x-5 items-center donut-chart-div">
-                                        <div className="flex items-center justify-center space-x-6 donut-chart-div ">
-                                            <DonutChart
-                                                data={sales}
-                                                category="sales"
-                                                index="name"
-                                                valueFormatter={dataFormatter}
-                                                colors={['blue', 'cyan', 'indigo', 'violet', 'fuchsia', 'red', 'green']}
-                                                className="w-40 donut-chart"
-                                            />
-                                            <Legend
-                                                categories={['Eau chaude', 'Électroménager', 'Cuisson', 'Audiovisuel', 'Éclairage', 'Autres', 'Chauffage']}
-                                                colors={['blue', 'cyan', 'indigo', 'violet', 'fuchsia', 'red', 'green']}
-                                                className="max-w-xs"
-                                            />
-
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                    </Modal.Body>
-                                </Modal>
-                            </div>
-
-                        </div>
+            <div className='overview-div-columns'>
+                <div className='overview-div-rows overview-row-3'>
+                    <h3 className='h3-overview'>Epargne</h3>
+                    <ProgressCircle value={65} size="xl" color="lime" showAnimation="true">
+                        <span className="percentage-battery-overview">65%</span>
                     </ProgressCircle>
-            </div>
-            <div className='overview-box overview-box2'>
-            <div className='overview-box2-info'>
-                <p>1582 W</p>
-                <img src={electricTower} alt="emoji" className=''></img>
-            </div>  
-            <CircularProgressbar
-                    value={40}
-                    className={`circular-progressbar2 mirrored-progress`}
-                    circleRatio={0.4}
-                    styles={{
-                        trail: {
-                            transform: "rotate(-162deg)", // Mantén la rotación original
-                            transformOrigin: "center center"
-                        },
-                        path: {
-                            transform: "rotate(-162deg)", // Mantén la rotación original
-                            transformOrigin: "center center",
-                            stroke: "#DE714C"
-                        },
-                        text: {
-                            fill: "#ddd"
-                        }
-                    }}
-                    strokeWidth={5}
-                />
+                    <p className='p-savings'>€18,5 / €27</p>
+                    <BarChart
+                        data={data}
+                        index="date"
+                        categories={['Epargne']}
+                        colors={["green"]}
+                        valueFormatter={dataFormatter}
+                        yAxisWidth={60}
+                        className="mt-6 hidden h-60 sm:block main-chart chart-overview"
+                        />
+                </div>
 
             </div>
         </div>
+
     );
 }
 
